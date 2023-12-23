da5ad86931f5d803eda17607a7d9661dbb9c9ac8
Ajay Biswas <104336525+BlueSquare4@users.noreply.github.com>
2023-12-23 10:46:38 +0530

    Update app.py with CORS

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 import pyrebase
 from flask_socketio import SocketIO
 import firebase_admin
 from firebase_admin import credentials, db
 
 app = Flask(__name__)
+CORS(app)
 socketio = SocketIO(app)
 
 # Firebase initialization
